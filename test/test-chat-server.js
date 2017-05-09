baf3734c9e9dc423a55c737528925a6cccdf630d
emmpak <eiliev05@yahoo.com>
2017-05-09 10:53:40 +0100

    Add broadcasting test

---
+++ test/test-chat-server.js
@@ -0,0 +1,34 @@
+var chai = require('chai'),
+    mocha = require('mocha'),
+    should = chai.should();
+
+
+var io = require('socket.io-client'),
+    server = require('../index');
+
+var socketURL = 'http://localhost:3000';
+
+var options = {
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+var chatUser1 = {'name': 'Sean'};
+
+describe("Chat Server", function() {
+  /* Test 1 - A single user */
+  it('should broadcast new user once they connect', function(done){
+    var client = io.connect(socketURL, options);
+
+    client.on('connect', function(data){
+      client.emit('connection name', chatUser1);
+    });
+
+    client.on('new user', function(usersName) {
+      usersName.should.be.type('string');
+      usersName.should.equal(chatUser1.name + " has joined.");
+      client.disconnect();
+      done();
+    });
+  });
+});
