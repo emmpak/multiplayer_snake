e8627b87402cb9f6920c41ba96aaee9c3988d805
emmpak <eiliev05@yahoo.com>
2017-05-09 15:11:10 +0100

    Add message test

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ io.on('connect', function(socket){
   socket.on('connection name', function(user){
     io.emit('new user', user.name + " has joined.");
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('message', function(msg){
+    io.emit('message', msg);
   });
 });
 
