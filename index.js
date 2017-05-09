5043a9237b272c57d6e1206c0b1905b65962b545
emmpak <eiliev05@yahoo.com>
2017-05-09 18:36:24 +0100

    Set static folder and some move emit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use(express.static('public'));
+
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
@@ -13,6 +18,9 @@ io.on('connect', function(socket){
   });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+  socket.on('move', function(position){
+    io.emit('move', position);
   });
 });
 
--- public/javascript/move.js
+++ public/javascript/move.js
@@ -1,21 +1,26 @@
 $(function () {
   $(document).on('keydown', function(key){
-      move(key.which);
+      var position = move(key.which);
+      socket.emit('move', position);
     });
 
   function move(key) {
     switch(key) {
     case 40:
       $("#move").animate({top: "+=5"}, "fast");
+      $("#move").position();
       break;
     case 37:
       $("#move").animate({left: "-=5"}, "fast");
+      $("#move").position();
       break;
     case 39:
       $("#move").animate({left: "+=5"}, "fast");
+      $("#move").position();
       break;
     case 38:
       $("#move").animate({top: "-=5"}, "fast");
+      $("#move").position();
       break;
     }
   }
