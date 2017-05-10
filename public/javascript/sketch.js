d4268129e289e2a63b77b01a8fd0f1ce4e0af3d2
Simba14 <seanblundell@gmail.com>
2017-05-10 14:32:24 +0100

    Move square based on coordinates given

--- public/javascript/sketch.js
+++ public/javascript/sketch.js
@@ -1,38 +1,34 @@
-var squares = [];
+var square;
 
 function setup() {
   createCanvas(1000, 1000);
+  square = new Square();
+}
+
+function draw() {
   background(0, 0, 255);
-  squares[0] = new Square();
+  square.show();
 }
 
-function updateSquare(x, y) {
-  draw(x, y);
-}
-function removeSquare(){
-  squares.splice(0, 1);
+function getCoordinates(x, y) {
+  square.update(x, y);
 }
 
-function draw(x=20, y=20) {
-  for (var i = 0; i < squares.length; i++ ){
-    squares[i].move(x, y);
-    squares[i].display();
-  }
-}
 
 function Square() {
   this.x = 490;
   this.y = 490;
-  this.height = 20;
-  this.width = 20;
 
-  this.move = function(x, y) {
+  this.update = function(x, y) {
     this.x = x;
     this.y = y;
   };
-  this.display = function() {
-    rect(this.x, this.y, this.width, this.height);
+
+  this.show = function() {
+    fill(255);
+    rect(this.x,this.y,20,20);
   };
+
 }
 
 // socket.on('update position', function(coordinates){
