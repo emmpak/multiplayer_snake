39ee9e272438315a465bd5acec3bf061bb36a51b
emmpak <eiliev05@yahoo.com>
2017-05-10 10:55:59 +0100

    Add calculatePosition function

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var position = {x: 300, y:300};
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connect', function(socket){
-  socket.on('connection name', function(user){
-    io.emit('new user', user.name + " has joined.");
-  });
-  socket.on('message', function(msg){
-    io.emit('message', msg);
-  });
-  socket.on('move', function(position){
-    io.emit('move', position);
+  console.log('new connection ' + socket.id)
+  socket.on('move', function(key){
+    io.emit('move', calculatePosition(key));
   });
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+
+
+function calculatePosition(key) {
+  switch(key) {
+  case 40:
+    position[y] -= 5;
+  case 37:
+    position[x] += 5;
+  case 39:
+    position[x] -= 5;
+  case 38:
+    position[y] += 5;
+  default:
+    position;
+  }
+}
