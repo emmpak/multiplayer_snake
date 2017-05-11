e4e3b4a48267468fb3a78bd0aef43ea7171fa113
emmpak <eiliev05@yahoo.com>
2017-05-11 10:02:32 +0100

    Add new player and all players

--- index.js
+++ index.js
@@ -8,25 +8,50 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 var square = require('./calculatePosition')
-var position = {x:490, y:490};
-var globalKey = 1;
+http.lastPlayerID = 0;
+// var position = {x:490, y:490};
+// var globalKey = 1;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connect', function(socket){
-  setInterval(function(){
-    console.log("globalKey: " + globalKey);
-    io.emit('update position', square.calculatePosition(position,globalKey));
-  }, 500);
+  socket.on('new player', function() {
+    socket.player = {
+      id: http.lastPlayerID++,
+      x: randomInt(100,400),
+      y: randomInt(100,400)
+    };
+    socket.emit('all players', getAllPlayers());
+    socket.broadcast.emit('new player', socket.player);
+  });
+
+  function getAllPlayers() {
+    var players = [];
+    Object.keys(io.sockets.connected).forEach(function(socketID){
+      var player = io.sockets.connected[socketID].player;
+      if(player) players.push(player);
+    });
+    return players;
+  }
+
+  function randomInt (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+  //
+  // setInterval(function(){
+  //   console.log("globalKey: " + globalKey);
+  //   io.emit('update position', square.calculatePosition(position,globalKey));
+  // }, 500);
 
   console.log('new connection ' + socket.id)
 
   socket.on('keypress', function(key){
     console.log("Server received keypress: " + key);
-    globalKey = key;
-    io.emit('update position', square.calculatePosition(position,key));
+    // globalKey = key;
+    console.log(socket.player);
+    io.emit('update position', square.calculatePosition(socket.player, key));
   });
 });
 
--- public/javascript/sketch.js
+++ public/javascript/sketch.js
@@ -1,14 +1,16 @@
 var socket = io();
-var square;
+var squares =[];
 
 function setup() {
   createCanvas(1000, 1000);
-  square = new Square();
+  socket.emit('new player');
 }
 
 function draw() {
   background(0, 0, 255);
-  square.show();
+  for(var i=0; i<squares.length; i++){
+    squares[i].show();
+  }
 }
 
 function updatePosition(x, y) {
@@ -26,6 +28,16 @@ function Square() {
     rect(this.x,this.y,20,20);
   };
 }
+
+socket.on('new player', function(player){
+  squares.push(player);
+})
+
+socket.on('all players', function(players){
+  for(var i=0; i< players.length; i++) {
+    squares.push(players[i]);
+  }
+});
 
 socket.on('update position', function(coordinates){
   updatePosition(coordinates.x, coordinates.y);
