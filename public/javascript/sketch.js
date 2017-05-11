25585a59ec61044d3aa36dc31b32a0f7e5e5010c
Simba14 <seanblundell@gmail.com>
2017-05-11 11:44:01 +0100

    Changed update function to update squares array

--- public/javascript/sketch.js
+++ public/javascript/sketch.js
@@ -1,50 +1,51 @@
 var socket = io();
 var squares = [];
+var colours = ['white', 'red', 'green', 'pink'];
 
 function setup() {
   createCanvas(1000, 1000);
 }
 
 function draw() {
-  background(0, 0, 255);
+  background('#34495e');
   for(var i=0; i<squares.length; i++){
-    squares[i].show();
+    squares[i].show(i);
   }
 }
 
 function updatePosition(i, x, y) {
-  squares[i].update(x,y)
+  squares[i].update(x,y);
 }
 
 function Square() {
+
   this.update = function(x, y) {
     this.x = x;
     this.y = y;
   };
 
-  this.show = function() {
-    fill(255);
+  this.show = function(id) {
+    fill(colours[id]);
     rect(this.x,this.y,20,20);
   };
 }
 
-// socket.on('update position', function(coordinates){
-//   for(var i=0; i<coordinates.length; i++){
-//     updatePosition(coordinates.id, coordinates.position.x, coordinates.position.y);
-//   }
-// });
-//
-// socket.on('new player', function() {
-//   squares << new Square();
-// });
-
-function testingSocketNewSquare() {
-  squares.push(new Square());
-  console.log(squares);
-}
-
-function testingSocketUpdatePosition(coordinates) {
-  for(var i=0; i<coordinates.length; i++){
-    updatePosition(coordinates[i].id, coordinates[i].position.x, coordinates[i].position.y);
+socket.on('update position', function(coordinates){
+  while(coordinates.length > squares.length) {
+    squares.push(new Square());
   }
-}
+  for(var i=0; i<coordinates.length; i++){
+    updatePosition(coordinates.id, coordinates.position.x, coordinates.position.y);
+  }
+});
+
+// function testingSocketNewSquare() {
+//   squares.push(new Square());
+//   console.log(squares);
+// }
+//
+// function testingSocketUpdatePosition(coordinates) {
+//   for(var i=0; i<coordinates.length; i++){
+//     updatePosition(coordinates[i].id, coordinates[i].position.x, coordinates[i].position.y);
+//   }
+// }
