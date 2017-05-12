66b003fd0e6de6ebe3fbcbecdfead52d3acbabac
emmpak <eiliev05@yahoo.com>
2017-05-12 08:53:28 +0100

    Refactor show function and add on connect

--- public/javascript/sketch.js
+++ public/javascript/sketch.js
@@ -14,34 +14,41 @@ function draw() {
   }
 }
 
-function updatePosition(i, x, y) {
-  squares[i].update(x,y);
-}
+// function updatePosition(i, x, y) {
+//   squares[i].update(x,y);
+// }
 
 function Square() {
 
-  this.update = function(x, y) {
-    this.x = x;
-    this.y = y;
-  };
+  // this.update = function(x, y) {
+  //   this.x = x;
+  //   this.y = y;
+  // };
 
   this.show = function(id) {
     fill(colours[id]);
+    var square = squares.find(function(square) { return square.id === id });
+    for(var i=0; i<square.positions.length; i++) {
+      rect(square.x, square.y, 20,20);
+    }
     rect(this.x,this.y,20,20);
   };
 }
 
-socket.on('update position', function(coordinates){
-  while(coordinates.length >= squares.length) {
-    squares.push(new Square());
-  }
-  for(var i=0; i<coordinates.length; i++){
-    updatePosition(coordinates[i].id, coordinates[i].position.x, coordinates[i].position.y);
-  }
-});
+socket.on('connect', function(){
+  squares.push(new Square());
+  console.log(squares);
+})
+
+// socket.on('update position', function(coordinates){
+//   while(coordinates.length >= squares.length) {
+//     squares.push(new Square());
+//   }
+//   // for(var i=0; i<coordinates.length; i++){
+//   //   updatePosition(coordinates[i].id, coordinates[i].position.x, coordinates[i].position.y);
+//   // }
+// });
 
 socket.on('disconnect', function(id){
-  console.log(squares);
   squares = squares.filter(function(square) {square.id !== id});
-  console.log(squares);
 });
